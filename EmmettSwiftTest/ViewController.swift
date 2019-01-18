05aae021c0e1f7910a1e46672ca7fbd43ae1eaef
Emmett Tsai <emmett.tsai@gmail.com>
2019-01-19 02:38:51 +0800

    Test EmmettSwiftKit

--- EmmettSwiftTest/ViewController.swift
+++ EmmettSwiftTest/ViewController.swift
@@ -4,12 +4,18 @@
 //
 
 import UIKit
+import EmmettSwiftKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let myKit = EmmettTest()
+        myKit.hello()
+        myLabel.text = String(myKit.add(a: 1, b: 2))
     }
 
 
