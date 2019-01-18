980b10db3400945544f63620d1085b2fbd4443be
Emmett Tsai <emmett.tsai@gmail.com>
2019-01-19 01:26:48 +0800

    Initial Commit

---
+++ EmmettSwiftTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Emmett Tsai on 2019/1/19.
+//  Copyright © 2019 Emmett Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
